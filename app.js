0dd423eb24c241b165d44883d1e940766f3666d5
saadweb <49939483+saadweb@users.noreply.github.com>
2021-10-24 16:27:54 +0100

    progresse bar animation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const section = document.querySelector('.section');
 const scrollBar = document.querySelector('.scrollBar');
 animateBar = ()=>{
+    
     let width = (-section.getBoundingClientRect().top/(section.getBoundingClientRect().height-document.documentElement.clientHeight))*100;
     if (width < 0) {
         width=0;
